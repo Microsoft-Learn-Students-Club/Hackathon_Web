de682ce902a22fcfc764a3461c765ae97aa76418
achrekarom12 <om.201173105@vcet.edu.in>
2023-11-30 02:06:34 +0530

    Commit 8

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,21 @@ import React, { useState, useEffect } from "react";
 import logo from "../assets/Logo.png";
 import "./style/home.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faInstagram, faLinkedin, faDiscord } from "@fortawesome/free-brands-svg-icons";
+import { faAngleUp } from "@fortawesome/free-solid-svg-icons";
+import {
+  faInstagram,
+  faLinkedin,
+  faDiscord,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Home = () => {
   const targetDate = new Date("February 17, 2024 00:00:00 GMT+0530").getTime();
   const [timeRemaining, setTimeRemaining] = useState(calculateTimeRemaining());
+  const [showScrollButton, setShowScrollButton] = useState(false);
+
+  const handleRegistrationClick = () => {
+    window.location.href = "https://www.google.com"; // Replace with your actual registration link
+  };
 
   function calculateTimeRemaining() {
     const currentDate = new Date().getTime();
@@ -40,6 +50,26 @@ const Home = () => {
   }
 
   useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      setShowScrollButton(scrollY > 100); // Adjust the value based on when you want the button to appear
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
     const timer = setInterval(() => {
       setTimeRemaining(calculateTimeRemaining());
     }, 1000);
@@ -48,30 +78,75 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="bg-black p-4 md:p-8 text-center h-screen flex flex-col justify-center items-center">
-      <h1 className="font-bold text-transparent text-4xl md:text-8xl bg-clip-text bg-gradient-to-l from-rose-950 via-purple-400 to-purple-800">
-        Welcome to MLSC Codefest: <br />{" "}
-        <img className="logo-img" src={logo} alt="Logo" />
+    <div className="bg-black p-4 md:p-8 text-center min-h-screen flex flex-col justify-center items-center">
+      <p className="text-white mt-6 text-2xl md:text-3xl font-semibold">
+        Welcome to
+      </p>
+      <h1 className="font-bold text-transparent text-3xl md:text-8xl mt-2 bg-clip-text bg-gradient-to-l from-rose-950 via-purple-400 to-purple-800">
+        MLSC Codefest <br />{" "}
+        <img className="logo-img mt-2" src={logo} alt="Logo" />
       </h1>
       <div className="flex space-x-4 mt-6">
-        <a href="https://instagram.com/mlsc_vcet?igshid=OGQ5ZDc2ODk2ZA==" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faInstagram} style={{ color: "#ffffff", fontSize: "2rem" }} />
+        <a
+          href="https://instagram.com/mlsc_vcet?igshid=OGQ5ZDc2ODk2ZA=="
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon
+            icon={faInstagram}
+            style={{ color: "#ffffff", fontSize: "1.5rem" }}
+          />
         </a>
-        <a href="https://www.linkedin.com/company/mlsc-vcet/mycompany/" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faLinkedin} style={{ color: "#ffffff", fontSize: "2rem" }} />
+        <a
+          href="https://www.linkedin.com/company/mlsc-vcet/mycompany/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            style={{ color: "#ffffff", fontSize: "1.5rem" }}
+          />
         </a>
-        <a href="https://discord.com/" target="_blank" rel="noopener noreferrer">
-          <FontAwesomeIcon icon={faDiscord} style={{ color: "#ffffff", fontSize: "2rem" }} />
+        <a
+          href="https://discord.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon
+            icon={faDiscord}
+            style={{ color: "#ffffff", fontSize: "1.5rem" }}
+          />
         </a>
       </div>
-      <div className="text-lg md:text-2xl text-white mt-4 md:mt-10">
+      <div className="text-lg md:text-2xl text-white mt-4 md:mt-8">
         <p>
-          <span className="text-6xl">{timeRemaining.days}</span> Days &nbsp;
-          <span className="text-6xl">{timeRemaining.hours}</span> Hours &nbsp;
-          <span className="text-6xl">{timeRemaining.minutes}</span>Minutes and&nbsp;
-          <span className="text-6xl">{timeRemaining.seconds}</span> Seconds left <br />
+          <span className="text-3xl md:text-6xl">{timeRemaining.days}</span>{" "}
+          Days &nbsp;
+          <span className="text-3xl md:text-6xl">{timeRemaining.hours}</span>{" "}
+          Hours &nbsp;
+          <span className="text-3xl md:text-6xl">{timeRemaining.minutes}</span>{" "}
+          Minutes and&nbsp;
+          <span className="text-3xl md:text-6xl">{timeRemaining.seconds}</span>{" "}
+          Seconds left <br />
           The Final Countdown to Codefest Thrills! 🚀
         </p>
+        <button
+          onClick={handleRegistrationClick}
+          className="bg-yellow-500 hover:bg-yellow-400 text-purple-800 font-semibold py-2 px-4 rounded-full mt-6"
+        >
+          Register Now
+        </button>
+        {showScrollButton && (
+          <button
+            onClick={scrollToTop}
+            className="fixed bottom-6 right-6 bg-purple-500 hover:bg-purple-400 bg-opacity-70 text-white font-bold py-2 px-4 rounded-full"
+          >
+            <FontAwesomeIcon
+              icon={faAngleUp}
+              style={{ color: "#ffffff", fontSize: "1.5rem" }}
+            />
+          </button>
+        )}
       </div>
     </div>
   );
